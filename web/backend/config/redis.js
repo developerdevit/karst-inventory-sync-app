067d95c0e9d68157bde3b61c25584fcc0e8ffb9d
Gennadiy Mariev <gennadiy.mariev@devit.group>
2024-02-07 10:49:21 +0200

    fix redis

--- web/backend/config/redis.js
+++ web/backend/config/redis.js
@@ -1,4 +1,4 @@
-import redis from 'redis';
+import Redis from 'ioredis';
 import pkg from 'bullmq';
 import { RedisSessionStorage } from '@shopify/shopify-app-session-storage-redis';
 
@@ -8,35 +8,38 @@ import logger from './logger.js';
 
 const { Worker, Queue, RedisConnection } = pkg;
 
-let redisClient, connection, sessionStorage;
+let redisClient, sessionStorage;
 
 const host = REDIS_URL.split('@')[1].split(':')[0];
 const port = parseInt(REDIS_URL.split('@')[1].split(':')[1]);
+const password = REDIS_URL.split('@')[0].split(':')[2];
 
-console.log('REDIS_URL', REDIS_URL, host, port);
+console.log('REDIS_URL', REDIS_URL, host, port, password);
+
+const connection = {
+  host,
+  port,
+  password,
+};
 
 try {
-  redisClient = await redis
-    .createClient({
-      url: REDIS_URL,
-    })
-    .on('error', (err) => console.log('Redis Client Error', err))
-    .connect();
-  // redisClient = new Redis(REDIS_URL, {
-  //   maxRetriesPerRequest: null,
-  //   tls: {
-  //     rejectUnauthorized: false,
-  //   },
-  // });
+  // redisClient = await redis
+  //   .createClient({
+  //     url: REDIS_URL,
+  //   })
+  //   .on('error', (err) => console.log('Redis Client Error', err))
+  //   .connect();
+  redisClient = new Redis(REDIS_URL, {
+    maxRetriesPerRequest: null,
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
   sessionStorage = new RedisSessionStorage(`${REDIS_URL}/1`);
 
   // redis://:p90689d458616548983786890e0a1f59b390b6721ffee1024598818ffcdb60232@ec2-3-248-40-254.eu-west-1.compute.amazonaws.com:31459
   // connection = new RedisConnection(redisClient);
-  connection = {
-    host,
-    port,
-  };
 } catch (error) {
   logger.error('redis connection error: ' + error);
   console.log('redis connection error: ', error);
